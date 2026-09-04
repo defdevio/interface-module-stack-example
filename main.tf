dd15c89158ff0150c91aeb591e2ec6877ed3962c
Tony Owens <anthony.owens1987@gmail.com>
2026-09-03 23:01:44 -0700

    Updated module version

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 module "application" {
-  source = "github.com/defdevio/interface-module-example?ref=v1.0.0"
+  source = "github.com/defdevio/interface-module-example?ref=v1.0.1"
 
   account_id                      = var.account_id
   aws_region                      = var.aws_region
